0b94b85e311da668557ceffeacd04035d6cff1c4
9631kunn <infourbh+1@gmail.com>
2021-01-07 18:43:49 +0900

    add: controller & routing

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,38 @@
+class LinebotController < ApplicationController
+  require "line/bot"
+
+  protect_from_forgery :except => [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_token = ENV["LINE_CHANNEL_CHANNEL_CHANNELID"]
+      config.channel_secret = ENV["LINE_CHANNEL_CHANNEL_CHANNELSECRET"]
+    }
+  end
+
+  def callback
+    body = requset.body.read
+    signature = requset.env["HTTP_X_LINE_SIGNATURE"]
+    unless client.validate_signature(body, signature)
+      head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: "text",
+            text: event.message["text"]
+          }
+        end
+      end
+    }
+    head :ok
+  end
+
+
+end
